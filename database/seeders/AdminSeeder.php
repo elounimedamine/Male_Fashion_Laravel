3f4256f37c8c10300eb598ef2cfbbb3c3a213220
Amine <medamineelouni1@gmail.com>
2023-01-19 03:54:11 +0100

    Seeding_Add_Admin_VD15

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'role' => 'admin',
+                'password' => Hash::make('123456789')
+            ]
+        );
+    }
+}
